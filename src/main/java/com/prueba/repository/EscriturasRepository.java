25a15e5388210d2c765ed297264706300aa2c2eb
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-04-26 10:36:06 -0500

    crea el repositorio

--- src/main/java/com/prueba/repository/EscriturasRepository.java
+++ src/main/java/com/prueba/repository/EscriturasRepository.java
@@ -1,8 +1,13 @@
 package com.prueba.repository;
 
+import com.prueba.Dto.EscriturasDto;
 import com.prueba.entity.Autores;
 import com.prueba.entity.Escrituras;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface EscriturasRepository extends JpaRepository<Escrituras, Long> {
+    List<EscriturasDto> findByGenero(String genero);
 }
