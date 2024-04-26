5d060fe88c03a05177dc71b95c35a3722e957736
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-04-26 10:34:13 -0500

    crea los endpoints guardar, listaTodo y buscar por genero

--- src/main/java/com/prueba/controller/EscriturasController.java
+++ src/main/java/com/prueba/controller/EscriturasController.java
@@ -1,4 +1,32 @@
 package com.prueba.controller;
 
+import com.prueba.Dto.EscriturasDto;
+import com.prueba.services.EscriturasService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/escrituras")
 public class EscriturasController {
+
+    @Autowired
+    EscriturasService escriturasService;
+
+    @PostMapping("/save")
+    public ResponseEntity<?> create(@RequestBody EscriturasDto escriturasDto){
+       return escriturasService.createEscrituras(escriturasDto);
+
+    }
+    @GetMapping
+    public ResponseEntity<?> List(){
+        return escriturasService.ListAll();
+
+    }
+    @GetMapping("/{genero}")
+    public ResponseEntity<?> ListGenero(@PathVariable String genero){
+        return escriturasService.ListGenero(genero);
+
+    }
+
 }
